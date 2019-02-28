c110a33b19ef0842d22761e8594c806c6c9f7efd
Mötz Jensen <motz@e-s.dk>
2019-02-28 15:18:54 +0100

    Create invoke-clientcredentialsgrant.ps1

---
+++ PSOAuthHelper/functions/invoke-clientcredentialsgrant.ps1
@@ -0,0 +1,25 @@
+﻿function Invoke-ClientCredentialsGrant {
+    [CmdletBinding()]
+    [OutputType()]
+    param (
+        [Parameter(Mandatory = $True, Position = 1)]
+        [string] $AuthProviderUri,
+
+        [Parameter(Mandatory = $true, Position = 2)]
+        [string] $Resource,
+
+        [Parameter(Mandatory = $true, Position = 3)]
+        [string] $GrantType,
+
+        [Parameter(Mandatory = $false, Position = 4)]
+        [string] $ClientId,
+
+        [Parameter(Mandatory = $false, Position = 5)]
+        [string] $ClientSecret,
+
+        [Parameter(Mandatory = $false, Position = 8)]
+        [string] $Scope
+    )
+
+    Invoke-Authorization @PSBoundParameters
+}
