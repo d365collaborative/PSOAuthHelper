aa9e8f1dd331eaa7980f187f16e405a0422999d7
Mötz Jensen <motz@e-s.dk>
2019-05-02 23:47:58 +0200

    Pester test updated

--- PSOAuthHelper/tests/functions/Get-BearerToken.Tests.ps1
+++ PSOAuthHelper/tests/functions/Get-BearerToken.Tests.ps1
@@ -1,4 +1,4 @@
-﻿Describe "New-AuthorizationHeaderBearerToken Unit Tests" -Tag "Unit" {
+﻿Describe "Get-BearerToken Unit Tests" -Tag "Unit" {
 	BeforeAll {
 		# Place here all things needed to prepare for the tests
 	}
@@ -8,41 +8,41 @@
 	
 	Describe "Ensuring unchanged command signature" {
 		It "should have the expected parameter sets" {
-			(Get-Command New-AuthorizationHeaderBearerToken).ParameterSets.Name | Should -Be '__AllParameterSets'
+			(Get-Command Get-BearerToken).ParameterSets.Name | Should -Be '__AllParameterSets'
 		}
 		
-		It 'Should have the expected parameter URL' {
-			$parameter = (Get-Command New-AuthorizationHeaderBearerToken).Parameters['URL']
-			$parameter.Name | Should -Be 'URL'
-			$parameter.ParameterType.ToString() | Should -Be System.String
+		It 'Should have the expected parameter InputObject' {
+			$parameter = (Get-Command Get-BearerToken).Parameters['InputObject']
+			$parameter.Name | Should -Be 'InputObject'
+			$parameter.ParameterType.ToString() | Should -Be System.Management.Automation.PSObject
 			$parameter.IsDynamic | Should -Be $False
 			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
 			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
 			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $True
 			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 1
-			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $True
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
-		It 'Should have the expected parameter BearerToken' {
-			$parameter = (Get-Command New-AuthorizationHeaderBearerToken).Parameters['BearerToken']
-			$parameter.Name | Should -Be 'BearerToken'
-			$parameter.ParameterType.ToString() | Should -Be System.String
+		It 'Should have the expected parameter Raw' {
+			$parameter = (Get-Command Get-BearerToken).Parameters['Raw']
+			$parameter.Name | Should -Be 'Raw'
+			$parameter.ParameterType.ToString() | Should -Be System.Management.Automation.SwitchParameter
 			$parameter.IsDynamic | Should -Be $False
 			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
 			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
-			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $True
-			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be 2
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be -2147483648
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
-			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $True
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
 	}
 	
 	Describe "Testing parameterset __AllParameterSets" {
 		<#
-		__AllParameterSets -URL -BearerToken
-		__AllParameterSets -URL -BearerToken
+		__AllParameterSets -InputObject
+		__AllParameterSets -InputObject -Raw
 		#>
 	}
 
