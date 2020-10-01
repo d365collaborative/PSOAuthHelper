e46a57f0a9f93a7a19fa63a97d8cd66198db7536
Mötz Jensen <motz@e-s.dk>
2020-10-01 20:33:00 +0200

    Fix: Pester issues

--- build/vsts-validate.ps1
+++ build/vsts-validate.ps1
@@ -4,4 +4,6 @@
 # Needs to ensure things are Done Right and only legal commits to master get built
 
 # Run internal pester tests
+
+Import-Module "Pester" -MaximumVersion 4.99.99 -Force
 & "$PSScriptRoot\..\PSOAuthHelper\tests\pester.ps1"
