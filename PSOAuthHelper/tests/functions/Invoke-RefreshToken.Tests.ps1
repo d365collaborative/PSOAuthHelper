e2a9549ab4a6543656291ab354b25c27b5aace9e
Mötz Jensen <motz@e-s.dk>
2020-10-01 20:13:04 +0200

    Tests: Parameter unit tests updated

--- PSOAuthHelper/tests/functions/Invoke-RefreshToken.Tests.ps1
+++ PSOAuthHelper/tests/functions/Invoke-RefreshToken.Tests.ps1
@@ -75,18 +75,31 @@
 			$parameter.ParameterSets['Object'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['Object'].ValueFromRemainingArguments | Should -Be $False
 		}
+		It 'Should have the expected parameter EnableException' {
+			$parameter = (Get-Command Invoke-RefreshToken).Parameters['EnableException']
+			$parameter.Name | Should -Be 'EnableException'
+			$parameter.ParameterType.ToString() | Should -Be System.Management.Automation.SwitchParameter
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be -2147483648
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
+		}
 	}
 	
 	Describe "Testing parameterset Object" {
 		<#
 		Object -AuthProviderUri -ClientId
-		Object -AuthProviderUri -ClientId -InputObject
+		Object -AuthProviderUri -ClientId -InputObject -EnableException
 		#>
 	}
  	Describe "Testing parameterset Simple" {
 		<#
 		Simple -AuthProviderUri -ClientId -RefreshToken
-		Simple -AuthProviderUri -ClientId -RefreshToken
+		Simple -AuthProviderUri -ClientId -RefreshToken -EnableException
 		#>
 	}
 
