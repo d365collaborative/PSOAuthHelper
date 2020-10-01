33858703def0986ba040528c69d8044dc63a8fcc
Mötz Jensen <motz@e-s.dk>
2020-10-01 20:17:29 +0200

    Fix: Pester and ReviewUnusedParameter

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -1,7 +1,9 @@
-$modules = @("Pester", "PSFramework", "PSScriptAnalyzer")
+$modules = @("PSFramework", "PSScriptAnalyzer")
 
 foreach ($module in $modules) {
     Write-Host "Installing $module" -ForegroundColor Cyan
     Install-Module $module -Force -SkipPublisherCheck
     Import-Module $module -Force -PassThru
 }
+
+Import-Module "Pester" -MaximumVersion 4.99.99 -Force
